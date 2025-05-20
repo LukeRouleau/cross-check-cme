02c259cfc3bedfb3411a9067c4d2c27baf4a774b
LukeRouleau <lucasluke2000@gmail.com>
2025-05-20 11:04:56 -0500

    format

--- src/app.d.ts
+++ src/app.d.ts
@@ -33,7 +33,7 @@ declare global {
 			context: {
 				waitUntil(promise: Promise<any>): void;
 			};
-			caches: CacheStorage & { default: Cache }
+			caches: CacheStorage & { default: Cache };
 		}
 	}
 }
